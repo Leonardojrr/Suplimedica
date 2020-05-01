e5e1c9d3daf20bf94e62f0fd4266f5ab95ceced8
Leonardo Rodrigues <leonardojrr1@gmail.com>
2020-05-01 19:36:52 -0400

    Settup the bases

---
+++ public/electron.js
@@ -0,0 +1,34 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1100,
+    height: 618.75,
+    minWidth: 1100,
+    minHeight: 619,
+    frame: false,
+  });
+  mainWindow.loadURL("http://localhost:3000");
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
