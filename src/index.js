96a509a50d4d849ab759cd781c30bf01dc2fa9c8
Leonardo Rodrigues <leonardojrr1@gmail.com>
2020-05-04 22:29:56 -0400

    Adding fonts to proyect

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import Root from "./Root";
 
 import "./index.css";
 
+//Fonts imports
+import "./assets/Fonts/Nunito/Nunito-Bold.ttf";
+import "./assets/Fonts/Nunito/Nunito-Regular.ttf";
+
 ReactDOM.render(<Root />, document.getElementById("root"));
