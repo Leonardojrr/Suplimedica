adc2d953ad69767bc3733c5bc29ce1d5de6bc955
Wisam <wisam_mozalbat@hotmail.com>
2020-08-10 04:55:12 -0400

    filer modules

--- src/Pages/module/People/People.js
+++ src/Pages/module/People/People.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import classes from "./People.module.css";
 import { NavLink, useRouteMatch, Switch, Route } from "react-router-dom";
+import { SessionContext } from "../../../context/SessionContext";
 
 import { FaTruck } from "react-icons/fa";
 import { BsFillPersonFill } from "react-icons/bs";
@@ -11,71 +12,100 @@ import Providers from "../Providers/Providers";
 
 const People = (props) => {
     let match = useRouteMatch();
+    const sessionContext = useContext(SessionContext);
+    const [validModules, setValidModules] = useState({
+        proveedores: false,
+        clientes: false,
+    });
+
+    const { modules } = sessionContext.user;
+
+    useEffect(() => {
+        let proveedores = false;
+        let clientes = false;
+        if (modules.includes(5)) {
+            proveedores = true;
+        }
+        if (modules.includes(6)) {
+            clientes = true;
+        }
+
+        setValidModules({ proveedores, clientes });
+    }, []);
 
     return (
         <React.Fragment>
             <Switch>
                 <Route exact path={`${match.url}`}>
                     <div className={classes.Container}>
-                        <NavLink
-                            to={`${match.url}/clients`}
-                            className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
-                        >
-                            <div className={classes.IconContainer}>
-                                <BsFillPersonFill className={classes.Icon} />
-                            </div>
+                        {validModules.clientes ? (
+                            <NavLink
+                                to={`${match.url}/clients`}
+                                className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
+                            >
+                                <div className={classes.IconContainer}>
+                                    <BsFillPersonFill
+                                        className={classes.Icon}
+                                    />
+                                </div>
 
-                            <div className={`${classes.DescriptionContainer}`}>
-                                <span
-                                    style={{
-                                        display: "block",
-                                        color: "#0F52AB",
-                                        fontSize: 30,
-                                    }}
+                                <div
+                                    className={`${classes.DescriptionContainer}`}
                                 >
-                                    CLIENTES
-                                </span>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            color: "#0F52AB",
+                                            fontSize: 30,
+                                        }}
+                                    >
+                                        CLIENTES
+                                    </span>
 
-                                <span
-                                    style={{
-                                        display: "block",
-                                        marginTop: 15,
-                                        color: "lightgrey",
-                                    }}
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            marginTop: 15,
+                                            color: "lightgrey",
+                                        }}
+                                    >
+                                        Ver lista de clientes
+                                    </span>
+                                </div>
+                            </NavLink>
+                        ) : null}
+                        {validModules.proveedores ? (
+                            <NavLink
+                                to={`${match.url}/providers`}
+                                className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
+                            >
+                                <div className={classes.IconContainer}>
+                                    <FaTruck className={classes.Icon} />
+                                </div>
+                                <div
+                                    className={`${classes.DescriptionContainer}`}
                                 >
-                                    Ver lista de clientes
-                                </span>
-                            </div>
-                        </NavLink>
-
-                        <NavLink
-                            to={`${match.url}/providers`}
-                            className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
-                        >
-                            <div className={classes.IconContainer}>
-                                <FaTruck className={classes.Icon} />
-                            </div>
-                            <div className={`${classes.DescriptionContainer}`}>
-                                <span
-                                    style={{
-                                        display: "block",
-                                        color: "#0F52AB",
-                                        fontSize: 30,
-                                    }}
-                                >
-                                    PROVEEDORES
-                                </span>
-                                <span
-                                    style={{
-                                        display: "block",
-                                        marginTop: 15,
-                                        color: "lightgrey",
-                                    }}
-                                >
-                                    Ver lista de proveedores
-                                </span>
-                            </div>
-                        </NavLink>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            color: "#0F52AB",
+                                            fontSize: 30,
+                                        }}
+                                    >
+                                        PROVEEDORES
+                                    </span>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            marginTop: 15,
+                                            color: "lightgrey",
+                                        }}
+                                    >
+                                        Ver lista de proveedores
+                                    </span>
+                                </div>
+                            </NavLink>
+                        ) : null}
                     </div>
                 </Route>
 
--- src/Pages/module/Warehouse/Warehouse.js
+++ src/Pages/module/Warehouse/Warehouse.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import classes from "./Warehouse.module.css";
 import { NavLink, useRouteMatch, Switch, Route } from "react-router-dom";
+import { SessionContext } from "../../../context/SessionContext";
 
 import { FaWarehouse } from "react-icons/fa";
 import { FaDatabase } from "react-icons/fa";
@@ -11,71 +12,99 @@ import Inventory from "../Inventory/Inventory";
 
 const Warehouse = (props) => {
     let match = useRouteMatch();
+    const sessionContext = useContext(SessionContext);
+    const [validModules, setValidModules] = useState({
+        inventario: false,
+        productos: false,
+    });
+
+    const { modules } = sessionContext.user;
+
+    useEffect(() => {
+        let inventario = false;
+        let productos = false;
+        if (modules.includes(1)) {
+            inventario = true;
+        }
+        if (modules.includes(2)) {
+            productos = true;
+        }
+
+        setValidModules({ inventario, productos });
+    }, []);
 
     return (
         <React.Fragment>
             <Switch>
                 <Route exact path={`${match.url}`}>
                     <div className={classes.Container}>
-                        <NavLink
-                            to={`${match.url}/inventory`}
-                            className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
-                        >
-                            <div className={classes.IconContainer}>
-                                <FaWarehouse className={classes.Icon} />
-                            </div>
+                        {validModules.inventario ? (
+                            <NavLink
+                                to={`${match.url}/inventory`}
+                                className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
+                            >
+                                <div className={classes.IconContainer}>
+                                    <FaWarehouse className={classes.Icon} />
+                                </div>
 
-                            <div className={`${classes.DescriptionContainer}`}>
-                                <span
-                                    style={{
-                                        display: "block",
-                                        color: "#0F52AB",
-                                        fontSize: 30,
-                                    }}
+                                <div
+                                    className={`${classes.DescriptionContainer}`}
                                 >
-                                    INVENTARIO
-                                </span>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            color: "#0F52AB",
+                                            fontSize: 30,
+                                        }}
+                                    >
+                                        INVENTARIO
+                                    </span>
 
-                                <span
-                                    style={{
-                                        display: "block",
-                                        marginTop: 15,
-                                        color: "lightgrey",
-                                    }}
-                                >
-                                    Ver productos en el inventario
-                                </span>
-                            </div>
-                        </NavLink>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            marginTop: 15,
+                                            color: "lightgrey",
+                                        }}
+                                    >
+                                        Ver productos en el inventario
+                                    </span>
+                                </div>
+                            </NavLink>
+                        ) : null}
 
-                        <NavLink
-                            to={`${match.url}/products`}
-                            className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
-                        >
-                            <div className={classes.IconContainer}>
-                                <FaDatabase className={classes.Icon} />
-                            </div>
-                            <div className={`${classes.DescriptionContainer}`}>
-                                <span
-                                    style={{
-                                        display: "block",
-                                        color: "#0F52AB",
-                                        fontSize: 30,
-                                    }}
+                        {validModules.productos ? (
+                            <NavLink
+                                to={`${match.url}/products`}
+                                className={`${classes.ButtonContainer} ${classes.ButtonActive}`}
+                            >
+                                <div className={classes.IconContainer}>
+                                    <FaDatabase className={classes.Icon} />
+                                </div>
+                                <div
+                                    className={`${classes.DescriptionContainer}`}
                                 >
-                                    PRODUCTOS INTERNOS
-                                </span>
-                                <span
-                                    style={{
-                                        display: "block",
-                                        marginTop: 15,
-                                        color: "lightgrey",
-                                    }}
-                                >
-                                    Ver todos los productos
-                                </span>
-                            </div>
-                        </NavLink>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            color: "#0F52AB",
+                                            fontSize: 30,
+                                        }}
+                                    >
+                                        PRODUCTOS INTERNOS
+                                    </span>
+                                    <span
+                                        style={{
+                                            display: "block",
+                                            marginTop: 15,
+                                            color: "lightgrey",
+                                        }}
+                                    >
+                                        Ver todos los productos
+                                    </span>
+                                </div>
+                            </NavLink>
+                        ) : null}
                     </div>
                 </Route>
 
